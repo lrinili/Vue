1e5e4baf242e6e401e99a83d5e1427dd9ec1c4b1
李元义 <742142833@qq.com>
2017-04-20 23:31:41 +0800

    oberver 模式

--- vue.js
+++ vue.js
@@ -3,46 +3,63 @@ var dep=[]
 var obj={
   name:'lly'
  }
-function defineReactive(obj,key,val){  
 
-var property = Object.getOwnPropertyDescriptor(obj,key)
-  if(property&&property.configurable===false){  
-  	return 
+
+  //观察擦者模式 为obj所有的属性 添加setter getter
+  function Observer(value){ 
+       this.value
+       this.walk(value)
   }
- var getter = property&&property.get //undfined 或者get()
- var setter = property&&property.set
- Object.defineProperty(obj,key,{
-    configurable:true,
-    enumerable:true,
-    get:function(){
-    	console.log('获取')
-        var value = getter ? getter.call(obj) : val;
+  Observer.prototype.walk = function(obj){
+    var keys =  Object.keys(obj)
 
-    	return value  
-     },
-    set:function(newVal){
-    	console.log('设置')
-    	 var value = getter ? getter.call(obj) : val;
-	      if(newVal === value) {
-	        return;
-	      }
-	      if(setter) {
-	        setter.call(obj, newVal);
-	      } else {
-	        val = newVal;
-	      }
-
+    for(var i = 0; i<keys.length;i++){
+      defineReactive(obj,keys[i],obj[keys[i]])
     }
+  }
 
+  function defineReactive(obj,key,val){  
 
+  var property = Object.getOwnPropertyDescriptor(obj,key)
+    if(property&&property.configurable===false){  
+    	return 
+    }
+   var getter = property&&property.get //undfined 或者get()
+   var setter = property&&property.set
+   Object.defineProperty(obj,key,{
+      configurable:true,
+      enumerable:true,
+      get:function(){
+      	console.log('获取')
+          var value = getter ? getter.call(obj) : val;
 
- })
+      	return value  
+       },
+      set:function(newVal){
+      	console.log('设置')
+      	 var value = getter ? getter.call(obj) : val;
+  	      if(newVal === value) {
+  	        return;
+  	      }
+  	      if(setter) {
+  	        setter.call(obj, newVal);
+  	      } else {
+  	        val = newVal;
+  	      }
+          observe(newVal)
+        }
+     })
+   }
 
+  function observe(value) {
+    if(!value || typeof value !== 'object') {
+      return;
+    }
+    return new Observer(value);
+  }
 
-}
-
-
-
-
-obj.key='111'
+observe(obj)
+console.log(obj.name)
+obj.key='22'
+observe(obj)
 console.log(obj.key)
